b0f72b66980c97d937be9a721052f519ac12726b
abdallamoha <35732805+abdallamoha@users.noreply.github.com>
2021-02-12 08:47:04 +0300

    refactor:main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.red,
           //scaffoldBackgroundColor: Color(0xffFFFFF0)),
           scaffoldBackgroundColor: Color(0xffFAFAFA)),
       home: ServiceType(),
