e28d9126e5f316b14bb54932855382e0d613b3c1
abdallamoha <35732805+abdallamoha@users.noreply.github.com>
2021-04-22 08:45:43 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
           primarySwatch: Colors.blue,
-          scaffoldBackgroundColor: Color(0xffFFFFF0)),
+          scaffoldBackgroundColor: Color(0xffFFFFFA)),
           scaffoldBackgroundColor: Color(0xffFAFAFA)),
       home: ServiceType(),
     );
