f47dac9a96a5c17124e9d7abb439bc7f90b83da2
abdallamoha <35732805+abdallamoha@users.noreply.github.com>
2021-05-19 20:45:43 +0300

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nanny App',
+      title: 'Nanny App.',
       home: MyMap(),
     );
   }
